9fadfe97e48d7a4b9c8e2f320bcc2c3e99b6b2b0
DarshPatel0108 <darshpatel1890@gmail.com>
2026-06-19 12:37:09 +0530

    database setup done

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({ path: './.env' });
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;(async ()=>{
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     console.log('Connected to MongoDB');
+    
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//   }
+// })
